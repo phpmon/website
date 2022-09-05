71781bb66484a843a0a9979ebb1efde0698adac3
Nico Verbruggen <mail@nicoverbruggen.be>
2022-09-05 12:14:02 +0200

    Set up redirects

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => view('homepage'));
+
+Route::get('/github', fn () => redirect()->to('https://github.com/nicoverbruggen/phpmon'));
+Route::get('/sponsor', fn () => redirect()->to('https://nicoverbruggen.be/sponsor'));
+Route::get('/donate', fn () => redirect()->to('https://nicoverbruggen.be/sponsor'));
