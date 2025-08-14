f203b0a8ce5413cdc553ce55cf6214c42b595902
Nico Verbruggen <mail@nicoverbruggen.be>
2025-08-14 13:37:54 +0200

    Open early access in new tab

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -26,7 +26,7 @@
 <div>
     <div class="bg-primary p-4 text-center text-white">
         Support PHP Monitor with a monthly sponsorship of $5 or more on GitHub for early access builds!
-        <a class="underline font-bold" href="/early-access/release-notes">Learn more</a>
+        <a class="underline font-bold" href="/early-access/release-notes" target="_blank">Learn more</a>
     </div>
     <div class="bg-dim flex">
         <header class="flex flex-col m-auto max-w-xl py-5 md:py-8 lg:py-10 p-4">
