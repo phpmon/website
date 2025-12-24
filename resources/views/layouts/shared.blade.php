6e1c7203891eea26e809478bcc1f7ab8a175818a
Nico Verbruggen <mail@nicoverbruggen.be>
2025-12-24 13:27:02 +0100

    Update favicon

--- resources/views/layouts/shared.blade.php
+++ resources/views/layouts/shared.blade.php
@@ -16,10 +16,10 @@
     <meta property="og:description" content="@yield('description')">
     <meta property="og:image" content="{{ url('/images/phpmon-social.png') }}">
     {{-- Favicons --}}
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
+    <link rel="icon" type="image/svg+xml" href="{{ url('/favicon/favicon.svg') }}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ url('/favicon/favicon-96x96.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ url('/favicon/apple-touch-icon.png') }}">
+    <link rel="manifest" href="{{ url('/favicon/site.webmanifest') }}">
     {{-- Umami --}}
     <script defer src="https://umami.nicoverbruggen.be/script.js" data-website-id="a94ced9c-2bdd-4a02-9c52-f1b8d34aba89"></script>
 </head>
