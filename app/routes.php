2745ed087663fda52642fdf44cae24827b68ef07
Nico Verbruggen <mail@nicoverbruggen.be>
2026-02-18 12:04:49 +0100

    Update FAQ redirect

--- app/routes.php
+++ app/routes.php
@@ -15,7 +15,7 @@ Route::get('/early-access/release-notes', \App\Http\ReleaseNotesController::clas
 collect([
     Redirection::named('github', '/github', 'https://github.com/nicoverbruggen/phpmon'),
     Redirection::named('releases', '/releases', 'https://github.com/nicoverbruggen/phpmon/releases'),
-    Redirection::named('faq', '/faq', 'https://github.com/nicoverbruggen/phpmon#%EF%B8%8F-faq--troubleshooting'),
+    Redirection::named('faq', '/faq', 'https://github.com/nicoverbruggen/phpmon?tab=readme-ov-file#%E2%80%8D%EF%B8%8F-faq--troubleshooting'),
     Redirection::named('sponsor', '/sponsor', 'https://nicoverbruggen.be/sponsor'),
     Redirection::named('sponsor.now', '/sponsor/now', 'https://nicoverbruggen.be/sponsor#pay-now'),
     Redirection::named('wiki.pre-release', '/prerelease-php', 'https://github.com/nicoverbruggen/phpmon/wiki/Supporting-pre-release-versions-of-PHP'),
